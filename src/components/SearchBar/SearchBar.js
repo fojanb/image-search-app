80270f05185693196cc93fb70bee82466e4971f3
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-25 11:31:34 -0700

    Add components folder and environment variables via dotenv dependency

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+const SearchBar = () => {
+  return (
+    <>
+      <input type="text" autoComplete="off" placeholder="Search image..." required />
+    </>
+  );
+};
+export default SearchBar;
--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,4 +1,12 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
 const App = () => {
-    return <h1>Kitty</h1>
-}
+    const {REACT_APP_ACCESS_KEY} = process.env;
+    console.log(REACT_APP_ACCESS_KEY)
+  return (
+    <>
+      <h1>Image Search App with React.js</h1>
+      <SearchBar />
+    </>
+  );
+};
 export default App;
